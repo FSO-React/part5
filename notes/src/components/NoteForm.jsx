66ccbfab530fb47725974c6afdefb5e56b4b140c
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-02-02 18:54:38 -0300

    playwright testing

--- notes/src/components/NoteForm.jsx
+++ notes/src/components/NoteForm.jsx
@@ -23,6 +23,7 @@ const NoteForm = ({ createNote }) => {
 
       <form onSubmit={addNote}>
         <input
+          data-testid="note-input"
           value={newNote}
           onChange={handleChange}
           placeholder='write note content here'
